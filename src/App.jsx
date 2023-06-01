9a73c4434745f8513461e0ce31aafc5a5665553f
Gerardo Mansilla <gerardoemansilla@gmail.com>
2023-06-01 20:44:20 -0300

    conexion con api para obtener frases de los simpsons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
+  useEffect(()=>{
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async() =>{
+    try{
+      setMostrarSpinner(true)
+      const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes"); //GET querie
+      const dato = await respuesta.json();
+      setPersonaje(dato[0]);
+      setMostrarSpinner(false);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  const componenteRenderizado = (mostrarSpinner)? (
+    <div className="my-5">
+          <Spinner animation="border" variant="primary" />
+        </div>
+    ) :
+    (<Frase personaje={personaje}/>);
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase />
-        <Button variant="warning" >
+        {componenteRenderizado}
+        <Button variant="warning" onClick={consultarAPI}>
           Obtener frase
         </Button>
       </Container>
